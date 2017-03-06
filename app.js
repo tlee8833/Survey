91db2eaa7dddd4ae38d6b075fef40a53bc466de1
Tony <tlee8833@gmail.com>
2017-03-06 21:13:30 +0000

    Add Post Shift Survey and clean the codes

--- app.js
+++ app.js
@@ -5,83 +5,119 @@ var express        = require("express"),
     mysql          = require("mysql"),
     util           = require("util"),
     tablet_id      = 'A'
-    
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection( {
   host     : 'localhost',
   user     : 'tlee8833',
   password : '',
   database : 'c9'
 });
 
-connection.connect(function(err){
+connection.connect(function(err) {
   if(!err) {
-    console.log("Database is connected ... \n");  
+    console.log("Database is connected ... \n");
   } else {
-    console.log("Error connecting database ... \n");  
+    console.log("Error connecting database ... \n");
   }
 });
- 
-app.get("/", function(req, res){
-    res.redirect("/survey1");
+
+app.get("/", function(req, res) {
+  res.redirect("/survey1");
 });
 
 app.get("/survey1", function(req, res) {
-    res.render("form1");
-    app.set('time_form1_start', new Date());
+  res.render("form1");
+  app.set('time_form1_start', new Date());
 });
 
 app.post("/survey1", function(req, res) {
-    console.log(req.body);
-    connection.query("INSERT INTO survey SET ?", req.body, function(err, res) {
-        if (err) 
-            console.log(err);
-//        console.log((util.inspect(res, {showHidden: false, depth: null})));
-        app.set('survey1_ID', res.insertId);
-        app.set('survey1', req.body); //res.insertId); 
-        
-        console.log(req.app.get('time_form1_start'));
-        var time_form1_start = req.app.get('time_form1_start');
-        var time_form1_submit = new Date();
-        connection.query("UPDATE survey SET Tablet_ID = ?, Time_form1_start = ?, Time_form1_submit = ? WHERE ID = ?", [tablet_id, time_form1_start, time_form1_submit, res.insertId], function(err, res) {
-            if (err) 
-                throw err;
-        });
-    });
+//   console.log(req.body);
+  connection.query("INSERT INTO survey SET ?", req.body, function(err, res) {
+    if (err) 
+      console.log(err);
+    app.set('survey1_ID', res.insertId);
+    app.set('survey1', req.body);
 
-    res.redirect("/survey2");
+    var time_form1_start = req.app.get('time_form1_start');
+    var time_form1_submit = new Date();
+    connection.query("UPDATE survey SET Tablet_ID = ?, Time_form1_start = ?, \
+      Time_form1_submit = ? WHERE ID = ?", [tablet_id, time_form1_start, time_form1_submit,
+      res.insertId], function(err, res) {
+      if (err) 
+        throw err;
+    });
+  });
+  
+  res.redirect("/survey2");
 });
 
 app.get("/survey2", function(req, res) {
-    var survey1 = req.app.get('survey1');
-    
-    res.render("form2", {survey1:survey1});
-    app.set('time_form2_start', new Date());
+  var survey1 = req.app.get('survey1');
+
+  res.render("form2", {survey1:survey1});
+  app.set('time_form2_start', new Date());
 });
 
 app.post("/survey2", function(req, res) {
-    var survey1_ID = req.app.get('survey1_ID');
-    var time_form2_start = req.app.get('time_form2_start');
-    var time_form2_submit = new Date();
-    app.set('time_form2_submit', time_form2_submit);
-    
-    connection.query("UPDATE survey SET Time_form2_start = ?, Time_form2_submit = ?, QA1_1 = ?, QA1_2 = ?, QA1_3 = ?, QA1_4 = ?, QA2 = ?, QA3 = ?, QA4 = ?, QA5 = ?, QA6 = ?, QA7_1 = ?, QA7_2 = ?, QA7_3 = ?, QA7_4 = ?, QA7_5 = ?, QA7_6 = ?, QA7_7 = ?, QA7_8 = ?, QA8 = ?, Comments_2 = ? WHERE ID = ?", [time_form2_start, time_form2_submit, req.body.QA1_1, req.body.QA1_2, req.body.QA1_3, req.body.QA1_4, req.body.QA2, req.body.QA3, req.body.QA4, req.body.QA5, req.body.QA6, req.body.QA7_1, req.body.QA7_2, req.body.QA7_3, req.body.QA7_4, req.body.QA7_5, req.body.QA7_6, req.body.QA7_7, req.body.QA7_8, req.body.QA8, req.body.Comments_2, survey1_ID], function(err, res) {
-        if (err) 
-            throw err;
+  var survey1_ID = req.app.get('survey1_ID');
+  var time_form2_start = req.app.get('time_form2_start');
+  var time_form2_submit = new Date();
+  app.set('time_form2_submit', time_form2_submit);
+
+  connection.query("UPDATE survey SET Time_form2_start = ?, Time_form2_submit = ?, \
+    QA1_1 = ?, QA1_2 = ?, QA1_3 = ?, QA1_4 = ?, QA2 = ?, QA3 = ?, QA4 = ?, QA5 = ?, \
+    QA6 = ?, QA7_1 = ?, QA7_2 = ?, QA7_3 = ?, QA7_4 = ?, QA7_5 = ?, QA7_6 = ?, QA7_7 = ?, \
+    QA7_8 = ?, QA8 = ?, Comments_2 = ? WHERE ID = ?", [time_form2_start, time_form2_submit,
+    req.body.QA1_1, req.body.QA1_2, req.body.QA1_3, req.body.QA1_4, req.body.QA2, req.body.QA3,
+    req.body.QA4, req.body.QA5, req.body.QA6, req.body.QA7_1, req.body.QA7_2, req.body.QA7_3,
+    req.body.QA7_4, req.body.QA7_5, req.body.QA7_6, req.body.QA7_7, req.body.QA7_8,
+    req.body.QA8, req.body.Comments_2, survey1_ID], function(err, res) {
+    if (err) 
+      throw err;
     });
-    res.redirect("/finish");
+  res.redirect("/finish");
 });
 
 app.get("/finish", function(req, res) {
-    var time = req.app.get('time_form2_submit');
-    res.render("finish", {time:time});
-    
+  var time = req.app.get('time_form2_submit');
+  res.render("finish", {time:time});
+});
+
+app.get("/survey3", function(req, res) {
+  res.render("form3");
+  app.set('time_form3_start', new Date());
+});
+
+app.post("/survey3", function(req, res) {
+  // console.log(req.body);
+  connection.query("INSERT INTO survey3 SET ?", req.body, function(err, res) {
+    if (err) 
+      console.log(err);
+      
+    var time_start = req.app.get('time_form3_start');
+    var time_submit = new Date();
+    app.set('time_form3_submit', time_submit);
+    connection.query("UPDATE survey3 SET Tablet_ID = ?, Time_start = ?, Time_submit = ? \
+      WHERE ID = ?", [tablet_id, time_start, time_submit, res.insertId],
+      function(err, res) {
+      if (err) 
+        throw err;
+    });
+  });
+
+  res.redirect("/finish3");
+});
+
+app.get("/finish3", function(req, res) {
+  var time = req.app.get('time_form3_submit');
+  res.render("finish3", {time:time});
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
-   console.log("The Server Has Started!");
+  console.log("The Server Has Started!");
 });
--- seeds.js
+++ seeds.js
@@ -9,32 +9,35 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
   if(!err) {
-    console.log("Database is connected ... \n");  
+    console.log("Database is connected ... \n");
   } else {
-    console.log("Error connecting database ... \n");  
+    console.log("Error connecting database ... \n");
   }
 });
 
-// connection.query('CREATE TABLE survey1(Submit_ID INT AUTO_INCREMENT PRIMARY KEY, Submission_time TIMESTAMP, BadgeID VARCHAR(10), Rounding_type VARCHAR(10), Floor VARCHAR(10), New_admit BOOLEAN, Q1 VARCHAR(10), Q2_1 VARCHAR(10), Q2_2 VARCHAR(10), Q2_3 VARCHAR(10), Q2_4 VARCHAR(10), Q3 INT, Q4 VARCHAR(10), Q5 VARCHAR(10), Q6 VARCHAR(10))', function(err,res){
-//   if(err) throw err;
-
-//   console.log('Created a table: ' + res);
-// });
-
-connection.query('CREATE TABLE survey(ID INT AUTO_INCREMENT PRIMARY KEY, Tablet_ID VARCHAR(10), BadgeID VARCHAR(20), Time_form1_start DATETIME, Time_form1_submit DATETIME, Time_form2_start DATETIME, Time_form2_submit DATETIME, Rounding_type VARCHAR(10), Floor VARCHAR(10), New_admit VARCHAR(10), Q1 VARCHAR(10), Q2_1 VARCHAR(10), Q2_2 VARCHAR(10), Q2_3 VARCHAR(10), Q2_4 VARCHAR(10), Q3 INT, Q4 VARCHAR(10), Q5 VARCHAR(10), Q6 VARCHAR(10), Q7 VARCHAR(10), Comments_1 VARCHAR(500), QA1_1 VARCHAR(10), QA1_2 VARCHAR(10), QA1_3 VARCHAR(10), QA1_4 VARCHAR(10), QA2 INT, QA3 VARCHAR(10), QA4 VARCHAR(10), QA5 VARCHAR(10), QA6 VARCHAR(10), QA7_1 VARCHAR(10), QA7_2 VARCHAR(10), QA7_3 VARCHAR(10), QA7_4 VARCHAR(10), QA7_5 VARCHAR(10), QA7_6 VARCHAR(10), QA7_7 VARCHAR(10), QA7_8 VARCHAR(10), QA8 INT, Comments_2 VARCHAR(500))', function(err,res){
-  if(err) throw err;
-
+connection.query('CREATE TABLE survey(ID INT AUTO_INCREMENT PRIMARY KEY, \
+  Tablet_ID VARCHAR(10), BadgeID VARCHAR(20), Time_form1_start DATETIME, \
+  Time_form1_submit DATETIME, Time_form2_start DATETIME, Time_form2_submit DATETIME, \
+  Rounding_type VARCHAR(10), Floor VARCHAR(10), New_admit VARCHAR(10), Q1 VARCHAR(10), \
+  Q2_1 VARCHAR(10), Q2_2 VARCHAR(10), Q2_3 VARCHAR(10), Q2_4 VARCHAR(10), Q3 INT, \
+  Q4 VARCHAR(10), Q5 VARCHAR(10), Q6 VARCHAR(10), Q7 VARCHAR(10), Comments_1 VARCHAR(500), \
+  QA1_1 VARCHAR(10), QA1_2 VARCHAR(10), QA1_3 VARCHAR(10), QA1_4 VARCHAR(10), QA2 INT, \
+  QA3 VARCHAR(10), QA4 VARCHAR(10), QA5 VARCHAR(10), QA6 VARCHAR(10), QA7_1 VARCHAR(10), \
+  QA7_2 VARCHAR(10), QA7_3 VARCHAR(10), QA7_4 VARCHAR(10), QA7_5 VARCHAR(10), \
+  QA7_6 VARCHAR(10), QA7_7 VARCHAR(10), QA7_8 VARCHAR(10), QA8 INT, Comments_2 VARCHAR(500))',
+  function(err,res) {
+  if(err) 
+    throw err;
   console.log('Created a table: ' + res);
 });
 
-// connection.query("INSERT INTO survey_try(char1, int5, b1, str1) VALUES ('c', '50', 'TRUE', 'hey')", function(err, res){
-//   if (err) throw err;
-  
-//   console.log('Inserted ' + res);
-// });
-
-// connection.query("SELECT * FROM survey_try", function(err, res){
-//   if (err) throw err;
-  
-//   console.log('Selected ' + res);
-// });
+connection.query('CREATE TABLE survey3(ID INT AUTO_INCREMENT PRIMARY KEY, \
+  Tablet_ID VARCHAR(10), BadgeID VARCHAR(20), Time_start DATETIME, Time_submit DATETIME, \
+  Position VARCHAR(10), Rounding_type VARCHAR(15), Floor VARCHAR(10), Q1 VARCHAR(10), \
+  Q2 VARCHAR(10), Q3_a VARCHAR(15), Q3_b VARCHAR(15), Q4 VARCHAR(15), Q5_a VARCHAR(15), \
+  Q5_b VARCHAR(15), Q5_c VARCHAR(15), Q5_d VARCHAR(15), Q5_e VARCHAR(15), Q5_f VARCHAR(15), \
+  Q6 INT, Q7 INT, Comments VARCHAR(500))', function(err,res) {
+  if(err) 
+    throw err;
+  console.log('Created a table: ' + res);
+});
